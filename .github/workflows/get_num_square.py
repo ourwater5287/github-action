5339319decced61b60c539f4f9fdbff6d3b68162
Sneak Rail <32605026+ourwater5287@users.noreply.github.com>
2023-04-04 12:49:58 +0900

    Create get_num_square.py

---
+++ .github/workflows/get_num_square.py
@@ -0,0 +1,12 @@
+import os
+
+num = os.environ.get("INPUT_NUM")
+if num:
+  try:
+    num = int(num)
+  except Exception:
+    exit('ERROR("{}")'.format(num))
+else:
+  num = 1
+ 
+print(f"::set-output name=num_squared::{num ** 2}")
